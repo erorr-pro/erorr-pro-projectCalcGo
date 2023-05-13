3fcd30fb8ea29d6e8de905f437461ace851fbfbe
erorr-pro <my.dalar@gmail.com>
2023-05-13 22:08:15 +0200

    Обработка ошибок ввода

--- main/main.go
+++ main/main.go
@@ -1,23 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+
+	// Создание нового сканера для чтения ввода пользователя
+	scanner := bufio.NewScanner(os.Stdin)
 
 	var operator string
 	var number1, number2 float64
 
 	// Запрос первого числа у пользователя
 	fmt.Print("Введите первое число: ")
-	fmt.Scanln(&number1)
+	for {
+		if scanner.Scan() {
+			input := scanner.Text()
+			if num, err := strconv.ParseFloat(input, 64); err == nil {
+				number1 = num
+				break
+			} else {
+				fmt.Println("Ошибка: введите корректное число")
+				fmt.Print("Введите первое число: ")
+			}
+		}
+	}
 
 	// Запрос второго числа у пользователя
 	fmt.Print("Введите второе число: ")
-	fmt.Scanln(&number2)
+	for {
+		if scanner.Scan() {
+			input := scanner.Text()
+			if num, err := strconv.ParseFloat(input, 64); err == nil {
+				number2 = num
+				break
+			} else {
+				fmt.Println("Ошибка: введите корректное число")
+				fmt.Print("Введите второе число: ")
+			}
+		}
+	}
 
 	// Запрос оператора у пользователя
 	fmt.Print("Введите оператор (+, -, * или /): ")
-	fmt.Scanln(&operator)
+	for {
+		if scanner.Scan() {
+			operator = scanner.Text()
+			if isValidOperator(operator) {
+				break
+			} else {
+				fmt.Println("Ошибка: некорректный оператор")
+				fmt.Print("Введите оператор (+, -, * или /): ")
+			}
+		}
+	}
 
 	// Вызов функции calculations для выполнения операции
 	result := calculations(number1, number2, operator)
@@ -54,4 +94,9 @@ func calculations(number1, number2 float64, operator string) float64 {
 
 	return result
 
+}
+
+// Функция проверяет, является ли оператор корректным (+, -, * или /)
+func isValidOperator(operator string) bool {
+	return operator == "+" || operator == "-" || operator == "*" || operator == "/"
 }
